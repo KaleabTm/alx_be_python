234e31c4c0186d762e4eabdc814aafc7d54f39be
KaleabTm <kaltamiru8@gmail.com>
2024-09-12 01:42:51 +0300

    arthmetic_Operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,7 @@
+# performs basic arithmetic operations with two predefined numbers
+number1=3
+number2=4
+print(number1+number2)
+print(number2-number1)
+print(number1*number2)
+print(number1/number2)
