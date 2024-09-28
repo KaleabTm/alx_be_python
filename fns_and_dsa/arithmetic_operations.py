d851c67cbb1ff49f0ba121dc07006c795bf5e5e4
KaleabTm <kaltamiru8@gmail.com>
2024-09-28 19:25:48 +0300

    another_commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,13 @@
-def perform_operation(num1,num2,operation):
-    if operation == "add":
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
         return num1 + num2
-    elif operation == "substract":
+    elif operation == 'subtract':
         return num1 - num2
-    elif operation == "multiply":
+    elif operation == 'multiply':
         return num1 * num2
-    elif operation == "divide":
-        if num2 != 0:
-            return num1 / num2
-        else:
-            return "Error: Division by zero is not allowed"
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Cannot divide by zero"
+        return num1 / num2
     else:
         return "Invalid operation"
-    
